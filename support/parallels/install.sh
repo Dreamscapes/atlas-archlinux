28e50a0c069e1ecd0b9a213bb716b1832fa53ef2
Robert Rossmann <rr.rossmann@me.com>
2015-03-29 11:24:40 +0200

    Parallels Tools does not require Python!

--- support/parallels/install.sh
+++ support/parallels/install.sh
@@ -10,10 +10,6 @@ mount /tmp/prl-tools-lin.iso /mnt/iso
 ln -sf /usr/lib/systemd/scripts/ /etc/init.d
 export def_sysconfdir=/etc/init.d
 
-# Install Python for the install script
-pacman -S --needed --noconfirm python2
-ln -sf /usr/bin/python2 /usr/bin/python
-
 # Fingers crossed...
 /mnt/iso/install --install-unattended
 
@@ -23,9 +19,7 @@ chown root:root /usr/lib/systemd/system/parallels-tools.service
 systemctl enable parallels-tools.service
 
 # Cleanup
-pacman -R --noconfirm --nosave python2
 umount /mnt/iso
 rm -rf /etc/init.d
-rm -rf /usr/bin/python
 rm -rf /mnt/iso
 rm -rf /var/log/parallels.log
