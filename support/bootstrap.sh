8392a4d9b781db25cebbd93c88960fa183a00e62
Robert Rossmann <rr.rossmann@me.com>
2015-04-17 13:53:21 +0200

    Simplify the boot command

---
+++ support/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### INSTALLATION BOOTSTRAP ###
+
+# The purpose of this file is to enable ssh service with a passwordless root account for Packer.
+# This only affects the installation ISO's booted system, not the final build.
+
+cat <<LIST >> /etc/ssh/sshd_config
+PermitRootLogin yes
+PermitEmptyPasswords yes
+LIST
+
+# sshd may not even be started, but this will take care of that...
+systemctl restart sshd.service
